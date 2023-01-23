e036bfdbda5d6cc32ee1ee1c4878f47b69ce408e
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2023-01-24 01:46:56 +0600

    setup dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-
+import routes from "./routes/routes";
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+    <RouterProvider router={routes} />
+    </>
   );
 }
 
---
+++ src/routes/routes.js
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/main/Main";
+import Home from "../pages/home/Home";
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default routes;
